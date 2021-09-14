781c887a877597ec5af9e015866410c1e4ecc803
DragonOfDissent <dvpsherman@gmail.com>
2021-09-14 14:26:02 -0500

    Updated Comments

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,7 @@
 fun main(){
     // Using println to describe me
     println("My name is David.")
-    println("I have was injured a lot when I was younger (by my own mistakes).")
+    println("I was injured a lot when I was younger (by my own mistakes).")
     println("I broke the growth plate in my right thumb.")
     println("That happened with a bike.")
     println("It's about half an inch shorter.")

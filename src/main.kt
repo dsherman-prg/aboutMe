98e4778c7d69bea88e9aff9f19e646fbd944dd6a
DragonOfDissent <dvpsherman@gmail.com>
2021-09-14 14:19:40 -0500

    Updated Comments

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,5 @@
 fun main(){
+    // Using println
     println("My name is David.")
     println("I have was injured a lot when I was younger (by my own mistakes).")
     println("I broke the growth plate in my right thumb.")
